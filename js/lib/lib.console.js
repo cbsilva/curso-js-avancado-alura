1fc536ac25975a7b2507e34bd954ba93c1c536a2
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-02-25 16:53:21 -0300

    alterando a forma de declaração de variaveis
    deixando de usar var (que é  um notacao global) para usar let (que é declaracao por bloco)

---
+++ js/lib/lib.console.js
@@ -0,0 +1,15 @@
+/**
+ * Forma de uso do console
+ * documentação:
+ * https://developer.mozilla.org/en-US/docs/Web/API/console#Outputting_text_to_the_console
+ */
+
+console.group('debugando codigo');
+console.log(' primeiro debug do código');
+console.groupEnd();
+
+console.group('tipo de console');
+console.warn('Teste');
+console.info("answer time");
+console.error("Click to continue");
+console.groupEnd();
