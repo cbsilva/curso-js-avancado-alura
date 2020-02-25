2bcd78ce441186b7b595d096796ba507e722e5f1
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-02-25 02:10:54 -0300

    refatorado a classe negociacao, aplicando o conceito de encapsulamento

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -1,16 +1,34 @@
 class Negociacao {
 
+    /**
+     * Por convencao todos os metodos privates 
+     * devem ter seus atributos iniciados com _ 'underline'
+     * Estes atributos não podem ser acessados pela aplicação
+     */
+
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     /**
-     * Metodo obtemVolume
+     * Metodo getVolume
      * Retorna o resultado de qtde * valor
      */
-    obtemVolume() {
-        return this.quantidade * this.valor;
+    getVolume() {
+        return this._quantidade * this._valor;
+    }
+
+    getData(){
+        return this._data;
+    }
+
+    getQuantidade(){
+        return this._quantidade;
+    }
+
+    getValor(){
+        return this._valor;
     }
 }
