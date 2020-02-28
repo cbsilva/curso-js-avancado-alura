07f1718a306c40cd9f3c605c4f56e94edd062632
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-02-28 01:20:39 -0300

    Aula03-A ligação entre as ações do usuário e o modelo
    Desenvolvido classe de Negociação Controller e aplicado conceitos de spread operator, no tratamento de datas

---
+++ js/app/controllers/NegociacaoController.js
@@ -0,0 +1,58 @@
+class NegociacaoController {
+
+    constructor (){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+        Object.freeze(this);
+    }
+
+
+    adiciona(event) {        
+
+        event.preventDefault();
+
+        /**
+         * tratando a data com spread operator
+         */
+        let data = new Date(...this._inputData.value.split('-')
+                                   .map(function(item, indice){
+                                       return item - indice % 2;
+                                   })
+        );
+    
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+        this.limparCampos();
+    }
+
+    limparCampos() {
+
+        let $ = document.querySelector.bind(document);
+
+        let campos = [
+            $('#data'),
+            $('#quantidade'),
+            $('#valor')
+        ];
+
+        campos[0].value = '';
+        campos[1].value = 1;
+        campos[2].value = 0;
+    
+        campos[0].focus();
+
+
+    }
+
+
+    
+}
