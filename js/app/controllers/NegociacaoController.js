69098f35b5bbc1e8dbd56e23b521c70ac4cfab79
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-03-03 00:10:56 -0300

    Aula04 - Nova Classe Lista Negociacoes
    criado classe para lista negociacoes

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -5,35 +5,50 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes  = new ListaNegociacoes();
 
         //Object.freeze(this);
     }
 
+    /**
+     * 
+     * @param {*} event
+     * Método adiciona negociacoes 
+     */
 
     adiciona(event) {        
 
-        event.preventDefault();       
-            
-    
-        let negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
+        event.preventDefault();     
 
-        console.log(negociacao);
-
-        console.log(DateHelper.dataParaTexto(negociacao.data)); 
-                        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._limpaFormulario();
+
+        console.log(this._listaNegociacoes.negociacoes);
+                           
     }
 
+    /**
+     * Método privado para criar negociacao
+     */
+    _criaNegociacao() {
+
+        return new Negociacao(
+               DateHelper.textoParaData(this._inputData.value),
+               this._inputQuantidade.value,
+               this._inputValor.value
+        );
+    }
+
+    /**
+     * Método privado para limpar formulário
+     */
+
     _limpaFormulario() {
+
         this._inputData.value       = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value      = 0.0;
 
-        this._inputData.focus();
-        
+        this._inputData.focus();       
     }    
 }
