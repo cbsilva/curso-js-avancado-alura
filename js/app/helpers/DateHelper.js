efb6746a5ef69851bb3fb48f6cc2961ac0fd20d6
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-03-02 21:59:48 -0300

    Aula04  - Expressões regulares
    aplicando expressões regulares nos metodos de data e refatorando o código da classe DateHelper

--- js/app/helpers/DateHelper.js
+++ js/app/helpers/DateHelper.js
@@ -4,17 +4,16 @@ class DateHelper {
         throw new Error('Esta classe não pode ser instanciada');        
     }
 
-    static textoParaData(texto) {
-        /**
-         * tratando a data com spread (...) operator
-         */
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    static dataParaTexto(data) {
+        console.log('Data para texto ', data);
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 
-    static dataParaTexto(data) {
-        return  data.getDate() + '/' 
-               + (data.getMonth() + 1) + '/' 
-               + data.getFullYear();
-
+    static textoParaData(texto) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Data informada deve ser no formato AAAA-MM-DD.');
+        
+        //tratando a data com spread (...) operator
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
