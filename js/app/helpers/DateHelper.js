abe0c138405be89e4965057d95cec7c8c413b909
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-03-03 00:25:10 -0300

    Aula04 - melhorando comentários
    incluindo comentários nos lugares corretos antes dos metodos

--- js/app/helpers/DateHelper.js
+++ js/app/helpers/DateHelper.js
@@ -4,11 +4,19 @@ class DateHelper {
         throw new Error('Esta classe não pode ser instanciada');        
     }
 
+    /**
+     * Metodo para converter um campo date em tipo string(texto)
+     * @param {date} data 
+     */
     static dataParaTexto(data) {
-        console.log('Data para texto ', data);
+        //Aplicando template string
         return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 
+    /**
+     * Metodo para converter data em formato string para tipo date
+     * @param {string} texto 
+     */
     static textoParaData(texto) {
         if (!/\d{4}-\d{2}-\d{2}/.test(texto))
             throw new Error('Data informada deve ser no formato AAAA-MM-DD.');
