b7ef7b78815639862c452e41d635017720ce3d7b
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-03-03 12:50:21 -0300

    Aula05-Totalizando volume

--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -23,19 +23,26 @@ class NegociacoesView {
                 </tr>
             </thead>            
             <tbody>
-                ${model.negociacoes.map(n => {
-    
-                    return `
+                ${model.negociacoes.map(n => `
                     <tr>
                         <td>${DateHelper.dataParaTexto(n.data)}</td>
                         <td>${n.quantidade}</td>
                         <td>${n.valor}</td>
                         <td>${n.volume}</td>
-                    </tr>`
-                }).join('')}
+                    </tr>
+                `).join('')}
             </tbody>
             
             <tfoot>
+                <td colspan="3"></td>
+                <td>${
+                    (function(){
+                        let total = 0;
+                        model.negociacoes.forEach(n => total += n.volume);
+                        return total;
+                    
+                    })()
+                }</td>
             </tfoot>
         </table>`;
     }
