ca63754df946b1521b97a06cd94652b151ac08f0
Cleberson Silva <cpaulo.andrade@gmail.com>
2020-03-05 16:21:41 -0300

    aplicando conceito de arrow functions
    melhorando ainda mais o código com arrow function

--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,11 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 class ListaNegociacoes {
 
-    constructor(contexto,armadilha) {
+    constructor(armadilha) {
 
         this._negociacoes = [];
-        this._armadilha = armadilha;
-        this._contexto = contexto;
+        this._armadilha = armadilha;        
     }
 
     /**
@@ -16,9 +15,8 @@ class ListaNegociacoes {
     adiciona(negociacao){
 
         this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
-
+        this._armadilha(this);
+       
     }
     
     /**
@@ -35,7 +33,6 @@ class ListaNegociacoes {
 
     esvazia(){
         this._negociacoes = [];
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);        
     }
 }
