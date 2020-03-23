cc6b2224da6055d6be9dad1515fca13a621fbb49
Cleberson <Silva>
2020-03-23 03:38:21 -0300

    convertendo registros em objeto, utilizando json.parte

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -68,9 +68,17 @@ class NegociacaoController {
             if (xhr.readyState == 4){
 
                 if (xhr.status == 200) {
-                    console.log('Obtendo as negociações do servidor.');
+
+                    JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+
+                    this._mensagem.texto = "Negociações importadas com sucesso.";
+
                 } else {
-                    console.log('Não foi possivel obter as negociações do servidor.');
+
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possivel obter as negociações do servidor.';
                 }
             }
 
