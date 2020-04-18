cc2aba98d64ea200a2c9dc7382278601a37f3dc0
Cleberson <Silva>
2020-04-18 03:23:30 -0300

    Module Pattern
    Aplicando conceito de module Pattern

--- js/app/services/ConnectionFactory.js
+++ js/app/services/ConnectionFactory.js
@@ -1,39 +1,67 @@
-class ConnectionFactory {
+/**
+ * comando para chamar classe no browser
+ * apagar depois dos testes
+ * ConnectionFactory.getConnection().then(connection => console.log(connection));
+ */
 
-    constructor(){
+ var ConnectionFactory = (function () {
 
-        throw new Error('Não é possivel criar instancias de ConnectionFactory');
-    }
+    /**
+     * declaracao de variaveis locais
+     */
+    var stores = ['negociacoes'];
+    var version = 6;
+    var dbName = 'aluraframe'
+    var connection = null;
+
+     //transforma classe em modulo
+     return class ConnectionFactory {
+     
+         constructor(){
+     
+             throw new Error('Não é possivel criar instancias de ConnectionFactory');
+         }     
+     
+         static getConnection(){
+     
+             return new Promise((resolve, reject) => {
+     
+                 let openRequest = window.indexedDB.open(dbName, version);
+     
+                 openRequest.onupgradeneeded = e => {
+                     ConnectionFactory._createStores(e.target.result);
+                 }
+                 
+                 openRequest.onsuccess = e => {
+     
+                     if (!connection) connection = e.target.result;
+                     resolve(connection);
+                     
+                 }
+                 
+                 openRequest.onerror = e => {
+                     console.log(e.target.error);
+                     
+                     reject(e.target.error.name);
+                 }
+                 
+             });
+             
+         }
+         
+         static _createStores(connection) {
+             stores.forEach(store => {
+                 if (connection.objectStoreNames.contains(store))
+                 connection.deleteObjectStore(store);
+                 
+                 connection.createObjectStore(store, {autoIncrement:true});                    
+             });
+     
+         }
+     
+     
+     }
+ }) ();
 
 
-    static getConnection(){
 
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open('aluraframe', 6);
-
-            openRequest.onupgradeneeded = e => {
-                if (e.target.result.objectStoreNames.contains('negociacoes'))
-                    e.target.result.deleteObjectStore('negociacoes');
-                
-                e.target.result.createObjectStore('negociacoes', {autoIncrement:true});
-
-            }
-
-            openRequest.onsuccess = e => {
-               resolve(e.target.result);
-
-            }
-
-            openRequest.onerror = e => {
-                console.log(e.target.error);
-
-                reject(e.target.error.name);
-            }
-
-        });
-
-    }
-
-
-}
