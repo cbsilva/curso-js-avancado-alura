a3fa56bb0542710e6b75eab8445960c4874b7609
Cleberson <Silva>
2020-04-21 01:53:19 -0300

    Atualizando httpService para ECMA2016
    Atualizando método http Service, para o ECMA2016

--- js/app/services/HttpService.js
+++ js/app/services/HttpService.js
@@ -1,45 +1,24 @@
 class HttpService {
 
+    _handlerErrors(res) {
+        if (!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest ();
-            
-            xhr.open('GET', url);       
-    
-            xhr.onreadystatechange = () => {
-    
-                if (xhr.readyState == 4){    
-                    if (xhr.status == 200) {    
-                        resolve(JSON.parse(xhr.responseText));
-                                      
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }    
-            }    
-            /*executa */
-            xhr.send();
-        });
+        return fetch(url)
+            .then(res => this._handlerErrors(res))
+            .then(res => res.json());
     }
 
     post(url, dado) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
+        
+        return fetch(url, {
+            headers: {'Content-type':'application/json'},
+            method: 'POST',
+            body: JSON.stringify(dado)
 
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('Content-type','application/json');
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4){    
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-
-            }
-            xhr.send(JSON.stringify(dado));
-        });
+        })
+        .then(res => this._handlerErrors(res));
     }
 }
